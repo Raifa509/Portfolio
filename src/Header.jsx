39701b80d0d04ff9e5721fb5ce2cfc6285fff560
Raifa509 <fathimathulraifanp@gmail.com>
2025-11-12 08:04:23 +0530

    landing page cpmpleted

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 function Header() {
   return (
     <>
-    <div className='h-10 bg-gray-950 flex items-center justify-between nova-font py-12 px-15 text-[25px]'>
+    <div className='h-10 bg-gray-950 text-cyan-300 flex items-center justify-between tektur-font py-12 px-15 text-[25px]'>
         <h1>Raifa<span className="text-accent ">&lt;/&gt;</span></h1>
         <FontAwesomeIcon icon={faBars}/>
     </div>
--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -3,6 +3,7 @@ import TextType from './TextType'
 import GradientText from './GradientText'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faDownload, faEnvelope, faEnvelopeOpen } from '@fortawesome/free-solid-svg-icons'
 
 function LandingPage() {
     return (
@@ -31,9 +32,56 @@ function LandingPage() {
                             cursorCharacter="_"
                         />
                     </div>
-                    <div className='px-5 py-2 mt-10 flex items-center justify-center'>
-                        <h1 className=''>Resume</h1>
-                        
+                    <div className='px-5 py-2 mt-8 flex items-center justify-center'>
+                        <a
+                            href="/Fathimathul Raifa NP_resume.pdf"  
+                            download 
+                            className="tektur-font
+                                    relative inline-flex items-center justify-center
+                                    px-5 py-2.5 rounded-lg font-semibold
+                                    text-white tracking-wide
+                                    bg-linear-to-r from-indigo-700 via-fuchsia-600 to-pink-400
+                                    shadow-[0_0_12px_rgba(139,92,246,0.4)]
+                                    hover:shadow-[0_0_16px_rgba(139,92,246,0.6)]
+                                    transition-all duration-300 hover:scale-110 
+                                    cursor-pointer me-4"
+                                                >
+                            Resume
+                            <FontAwesomeIcon icon={faDownload} className="ml-2" />
+                        </a>
+
+                        <div className="flex space-x-4 text-violet-200 mt-4 hover:">
+                            <a
+                                href="https://www.linkedin.com/in/fathimathulraifa-np"
+                                target="_blank"
+                                rel="noopener noreferrer"  // security best practice
+                            >
+                                <FontAwesomeIcon
+                                    icon={faLinkedin}
+                                    className="text-2xl text-cyan-300 animate-bounce-delay-1 transition-all duration-300 hover:drop-shadow-[0_0_10px_#15cde6] hover:text-cyan-400"
+                                />
+                            </a>
+                            <a
+                                href="https://github.com/Raifa509"
+                                target="_blank"
+                                rel="noopener noreferrer"  // security best practice
+                            >
+                                <FontAwesomeIcon
+                                    icon={faGithub}
+                                    className="text-2xl text-cyan-300 animate-bounce-delay-2 transition-all duration-300 hover:drop-shadow-[0_0_10px_#15cde6]"
+                                />
+                            </a>
+
+                            <a href="mailto:fathimathulraifanp@gmail.com">
+                                <FontAwesomeIcon
+                                    icon={faEnvelope}
+                                    className="text-2xl text-cyan-300 animate-bounce-delay-3 transition-all duration-300 hover:drop-shadow-[0_0_10px_#15cde6] hover:text-cyan-400"
+                                />
+                            </a>
+                        </div>
+
+
+
                     </div>
                 </div>
 
