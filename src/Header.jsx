754d4cc76f3377af84e47a2178fd3a648bf3875f
Raifa509 <fathimathulraifanp@gmail.com>
2025-11-12 12:35:13 +0530

    responsive landing page

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,4 @@
-import { faBars, faCode, faEnvelope, faHome, faInfoCircle, faPaperPlane, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faCode, faEnvelope, faFolderOpen, faHome, faInfoCircle, faPaperPlane, faPhone } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useState } from 'react'
 
@@ -7,14 +7,14 @@ function Header() {
 
   return (
     <>
-      <div className='h-10 bg-gray-950 text-cyan-300 flex items-center justify-between tektur-font py-12 px-15 text-[25px] tracking-[-1px]'>
-        <h1>Raifa<FontAwesomeIcon icon={faCode} className='ms-1'/></h1>
+      <div className='h-10 bg-gray-950 text-cyan-300 flex items-center justify-between tektur-font md:py-12 py-10 md:px-15 px-6 md:text-[25px] text-[20px] md:tracking-[-1px]'>
+        <h1>Raifa<FontAwesomeIcon icon={faCode} className='ms-1' /></h1>
         <FontAwesomeIcon icon={faBars} onClick={() => setModal(!modal)} />
       </div>
 
       <div
         className={`
-    fixed top-0 right-0 h-screen w-64
+    fixed top-0 right-0 h-full md:w-64 w-55
     backdrop-blur-lg
     bg-linear-to-b from-violet-600/30 to-cyan-400/30
     border-l border-white/30
@@ -27,28 +27,36 @@ function Header() {
           href="#home"
           className="text-white text-2xl font-semibold transition-transform duration-300 transform hover:scale-105 hover:text-cyan-300"
           onClick={() => setModal(false)}
-        ><FontAwesomeIcon icon={faHome} className='me-2'/>
+        ><FontAwesomeIcon icon={faHome} className='me-2' />
           Home
         </a>
         <a
           href="#about"
           className="text-white text-2xl font-semibold transition-transform duration-300 transform hover:scale-105 hover:text-cyan-300"
           onClick={() => setModal(false)}
-        ><FontAwesomeIcon icon={faInfoCircle} className='me-2'/>
+        ><FontAwesomeIcon icon={faInfoCircle} className='me-2' />
           About
         </a>
         <a
           href="#projects"
           className="text-white text-2xl font-semibold transition-transform duration-300 transform hover:scale-105 hover:text-cyan-300"
           onClick={() => setModal(false)}
-        ><FontAwesomeIcon icon={faCode} className='me-2'/>
+        ><FontAwesomeIcon icon={faFolderOpen} className='me-2' />
           Projects
         </a>
+        <a
+          href="#skills"
+          className="text-white text-2xl font-semibold transition-transform duration-300 transform hover:scale-105 hover:text-cyan-300"
+          onClick={() => setModal(false)}
+        ><FontAwesomeIcon icon={faCode} className='me-2' />
+          Skills
+        </a>
+        
         <a
           href="#contact"
           className="text-white text-2xl font-semibold transition-transform duration-300 transform hover:scale-105 hover:text-cyan-300"
           onClick={() => setModal(false)}
-        ><FontAwesomeIcon icon={faEnvelope} className='me-2'/>
+        ><FontAwesomeIcon icon={faEnvelope} className='me-2' />
           Contact
         </a>
       </div>
--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -8,21 +8,21 @@ import { faDownload, faEnvelope, faEnvelopeOpen } from '@fortawesome/free-solid-
 function LandingPage() {
     return (
         <>
-            <div className="grid grid-cols-2 text-white h-screen">
-                <div className="flex flex-col mt-50 text-center text-white ms-32">
-                    <h1 className="text-3xl font-light tracking-wide text-gray-300 tektur-font">
+            <div className="grid grid-cols-1 md:grid-cols-2 text-white md:min-h-screen mt-15 md:mt-0">
+                <div className="order-2 md:order-1 flex flex-col -mt-10 md:mt-50 text-center text-white md:ms-32">
+                    <h1 className="md:text-3xl text-2xl font-light tracking-wide text-gray-300 tektur-font">
                         Hi 👋, I'm
                     </h1>
                     <GradientText
                         colors={["#e879f9", "#a78bfa", "#22d3ee", "#a78bfa", "#e879f9"]}
                         animationSpeed={3}
                         showBorder={false}
-                        className="custom-class text-6xl leading-20 font-extrabold tektur-font"
+                        className="custom-class md:text-6xl text-4xl md:leading-20 leading-15 font-extrabold tektur-font"
                     >
                         Fathimathul Raifa NP
                     </GradientText>
 
-                    <div className="mt-2 text-2xl font-medium tracking-wide  text-gray-300 tektur-font">
+                    <div className="mt-2 md:text-2xl text-xl font-medium tracking-wide  text-gray-300 tektur-font">
                         <TextType
                             text={["Full-Stack Developer", "Frontend Developer", "Backend Developer"]}
                             typingSpeed={120}
@@ -32,10 +32,10 @@ function LandingPage() {
                             cursorCharacter="_"
                         />
                     </div>
-                    <div className='px-5 py-2 mt-8 flex items-center justify-center'>
+                    <div className='px-5 py-2 mt-8 flex items-center justify-center '>
                         <a
-                            href="/Fathimathul Raifa NP_resume.pdf"  
-                            download 
+                            href="/Fathimathul Raifa NP_resume.pdf"
+                            download
                             className="tektur-font
                                     relative inline-flex items-center justify-center
                                     px-5 py-2.5 rounded-lg font-semibold
@@ -45,7 +45,7 @@ function LandingPage() {
                                     hover:shadow-[0_0_16px_rgba(139,92,246,0.6)]
                                     transition-all duration-300 hover:scale-110 
                                     cursor-pointer me-4"
-                                                >
+                        >
                             Resume
                             <FontAwesomeIcon icon={faDownload} className="ml-2" />
                         </a>
@@ -88,8 +88,8 @@ function LandingPage() {
 
 
                 {/* image div */}
-                <div className="flex items-center justify-center">
-                    <div className="relative w-[400px] h-[400px] rounded-full -mt-20">
+                <div className=" order-1 md:order-2 flex items-center justify-center">
+                    <div className="relative md:w-[400px] w-[300px] h-[300px] md:h-[400px] rounded-full md:-mt-20">
 
                         <div className="absolute inset-0 rounded-full p-[3px] animate-glow-gradient">
                             <div className="w-full h-full rounded-full bg-linear-to-br from-violet-600 blur-[6px] opacity-70"></div>
